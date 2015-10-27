87a8dee153955b5ae0b91d9524be48dfb78f5d4a
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-10-27 09:51:53 +0000

    Add TGM Plugin Activation class to define plugins required by the theme.

--- wp-content/themes/layobservers/functions.php
+++ wp-content/themes/layobservers/functions.php
@@ -23,6 +23,7 @@ $roots_includes = array(
 	'lib/comments.php', // Custom comments modifications
 	'lib/scripts.php', // Scripts and stylesheets
 	'lib/extras.php', // Custom functions
+	'lib/plugins.php', // Required plugins
 //	'/lib/theme-options.php', // Theme options
 //	'/lib/ajax.php', // AJAX setup
 	'/lib/custom.php', // Custom functions
