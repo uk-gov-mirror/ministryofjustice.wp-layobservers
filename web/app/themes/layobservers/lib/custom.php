67a2707f332bd5f602ec0d026970a77c32a7009b
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-06-23 15:19:01 +0100

    Update OptionTree to latest version using composer, replacing versioned code in theme.

--- web/app/themes/layobservers/lib/custom.php
+++ web/app/themes/layobservers/lib/custom.php
@@ -33,14 +33,10 @@ function get_attachment_id_from_src( $image_src ) {
 load_template( trailingslashit( get_template_directory() ) . 'lib/meta-boxes.php' );
 
 /* Setup option tree */
-add_filter( 'ot_theme_mode', '__return_true' );
 add_filter( 'ot_show_pages', '__return_false' );
 add_filter( 'ot_show_new_layout', '__return_false' );
-add_filter( 'ot_use_theme_options', '__return_false' );
+add_filter( 'ot_use_theme_options', '__return_true' );
 add_filter( 'ot_header_version_text', '__return_null' );
-require_once (trailingslashit( get_template_directory() ) . 'option-tree/ot-loader.php');
-
-
 
 /* Display child pages on parent page  */
 
