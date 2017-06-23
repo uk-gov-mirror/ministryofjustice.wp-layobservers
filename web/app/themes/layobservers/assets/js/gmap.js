182ba958b10f5fc990bb0f7065bac204b488d0c2
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-06-23 14:56:25 +0100

    CSS and JS fixes.
    - Apply bootstrap tweaks and variables.
    - Restructure gmap.js to pass jshint.

--- web/app/themes/layobservers/assets/js/gmap.js
+++ web/app/themes/layobservers/assets/js/gmap.js
@@ -1,49 +1,6 @@
 (function($) {
 
 /*
-*  render_map
-*
-*  This function will render a Google Map onto the selected jQuery element
-*
-*  @type	function
-*  @date	8/11/2013
-*  @since	4.3.0
-*
-*  @param	$el (jQuery element)
-*  @return	n/a
-*/
-
-function render_map( $el ) {
-
-	// var
-	var $markers = $el.find('.marker');
-
-	// vars
-	var args = {
-		zoom		: 16,
-		center		: new google.maps.LatLng(0, 0),
-		mapTypeId	: google.maps.MapTypeId.ROADMAP
-	};
-
-	// create map
-	var map = new google.maps.Map( $el[0], args);
-
-	// add a markers reference
-	map.markers = [];
-
-	// add markers
-	$markers.each(function(){
-
-    	add_marker( $(this), map );
-
-	});
-
-	// center map
-	center_map( map );
-
-}
-
-/*
 *  add_marker
 *
 *  This function will add a marker to the selected Google Map
@@ -128,6 +85,49 @@ function center_map( map ) {
 		// fit to bounds
 		map.fitBounds( bounds );
 	}
+
+}
+
+/*
+ *  render_map
+ *
+ *  This function will render a Google Map onto the selected jQuery element
+ *
+ *  @type	function
+ *  @date	8/11/2013
+ *  @since	4.3.0
+ *
+ *  @param	$el (jQuery element)
+ *  @return	n/a
+ */
+
+function render_map( $el ) {
+
+  // var
+  var $markers = $el.find('.marker');
+
+  // vars
+  var args = {
+    zoom		: 16,
+    center		: new google.maps.LatLng(0, 0),
+    mapTypeId	: google.maps.MapTypeId.ROADMAP
+  };
+
+  // create map
+  var map = new google.maps.Map( $el[0], args);
+
+  // add a markers reference
+  map.markers = [];
+
+  // add markers
+  $markers.each(function(){
+
+    add_marker( $(this), map );
+
+  });
+
+  // center map
+  center_map( map );
 
 }
 
