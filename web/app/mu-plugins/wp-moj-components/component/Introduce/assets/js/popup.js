88c59b81ca2d996e5a09b20493e25e76ae382955
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-11-13 16:37:42 +0000

    Removed static plugin from mu-plugins

--- web/app/mu-plugins/wp-moj-components/component/Introduce/assets/js/popup.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).on('click', '.moj-intro-notice .notice-dismiss', function () {
-    jQuery.ajax({
-        url: ajaxurl,
-        data: {
-            action: 'popup_dismissed',
-
-        }
-    });
-});
