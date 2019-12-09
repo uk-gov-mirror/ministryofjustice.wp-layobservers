32e4b92b35ccb720547448166dc2051853725056
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2019-12-09 10:10:42 +0000

    Fix header nav

--- web/app/themes/layobservers/header.php
+++ web/app/themes/layobservers/header.php
@@ -10,37 +10,6 @@
 
     <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
-<header class="banner" role="banner">
-  <div class="container navbar-expand-md navbar navbar-default navbar-static-top flex-wrap">
-
-    <div id="branding" class="flex-fill">
-      <a href="<?php echo home_url(); ?>">
-        <img alt="Lay Observers Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/lo-logo.svg"
-             class="logo-svg"/>
-      </a>
-    </div>
-
-    <div id="search" class="align-self-start flex-fill flex-shrink-0">
-      <?php get_search_form(); ?>
-    </div>
-
-    <div class="navbar-header">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <nav class="navbar-collapse col-sm-12 collapse" role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
 <body <?php body_class(); ?>>
 
 <!--[if lt IE 8]>
@@ -48,3 +17,35 @@
     <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
 </div>
 <![endif]-->
+
+<header class="banner" role="banner">
+    <div class="container navbar-expand-md navbar navbar-default navbar-static-top flex-wrap">
+
+        <div id="branding" class="flex-fill">
+            <a href="<?php echo home_url(); ?>">
+                <img alt="Lay Observers Logo" src="<?php bloginfo('template_directory'); ?>/assets/img/lo-logo.svg"
+                     class="logo-svg"/>
+            </a>
+        </div>
+
+        <div id="search" class="align-self-start flex-fill flex-shrink-0">
+            <?php get_search_form(); ?>
+        </div>
+
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+
+        <nav class="navbar-collapse col-sm-12 collapse" role="navigation">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+            endif;
+            ?>
+        </nav>
+    </div>
+</header>
