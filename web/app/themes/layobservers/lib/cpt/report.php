7ca0eb6a9c1b06af8c18fb432cc604f02af2cbc7
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-07-02 09:05:16 +0100

    Update Components Plugin

--- web/app/themes/layobservers/lib/cpt/report.php
+++ web/app/themes/layobservers/lib/cpt/report.php
@@ -1,14 +1,33 @@
 <?php
 
-function report_post_init() {
+function report_post_init()
+{
     $args = array(
-      'public' => true,
-      'label'  => 'Reports',
-      'supports'      => array( 'title' ),
-      'has_archive'   => false,
-      'publicly_queryable'  => false,
+        'public' => true,
+        'label' => 'Reports',
+        'supports' => array('title'),
+        'has_archive' => false,
+        'publicly_queryable' => false,
     );
-    register_post_type( 'report', $args );
+    register_post_type('report', $args);
 }
 
-add_action( 'init', 'report_post_init' );
+add_action('init', 'report_post_init');
+
+function report_update_sitemap_link($url, $post)
+{
+    if ('report' == get_post_type($post)) {
+
+        $report_url = get_post_meta($post->ID, 'report-upload', true);
+
+
+        if (!empty($report_url)) {
+            $url = $report_url;
+        }
+
+    }
+
+    return $url;
+}
+
+add_filter('wsp_cpt_link', 'report_update_sitemap_link', 10, 2);
