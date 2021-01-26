a275f9e9b68ccd5c938d71cbb359120b2d995b39
Beverley Newing <bnewing92@hotmail.co.uk>
2021-01-26 10:22:11 +0000

    Remove AddThis widget
    The AddThis widget was bringing in lots of cookies, whihc we would have had to made toggleable and added into a new functionality section of the cookie banner. Looking at it, it looks a bit phishy, and we've already made the decision to remove it off of other sites previously.

--- web/app/themes/layobservers/footer.php
+++ web/app/themes/layobservers/footer.php
@@ -29,9 +29,5 @@ var google_remarketing_only = false;
 
 <?php wp_footer(); ?>
 
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-562f51153023de54" async="async"></script>
-
 </body>
 </html>
-
